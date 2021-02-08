10161e946026eda80d8967308f1c8adfbac8fa65
Siphalor <info@siphalor.de>
2021-02-08 18:36:15 +0100

    Fix indentation

--- src/main/java/de/siphalor/bigitemsduh/BigItemsDuh.java
+++ src/main/java/de/siphalor/bigitemsduh/BigItemsDuh.java
@@ -8,19 +8,19 @@ import org.apache.logging.log4j.Logger;
 
 public class BigItemsDuh implements ModInitializer {
 
-    public static Logger LOGGER = LogManager.getLogger();
+	public static Logger LOGGER = LogManager.getLogger();
 
-    public static final String MOD_ID = "big_items_duh";
-    public static final String MOD_NAME = "Big items, duh!";
+	public static final String MOD_ID = "big_items_duh";
+	public static final String MOD_NAME = "Big items, duh!";
 
-    @Override
-    public void onInitialize() {
-        log(Level.INFO, "Initializing");
-        //TODO: Initializer
-    }
+	@Override
+	public void onInitialize() {
+		log(Level.INFO, "Initializing");
+		//TODO: Initializer
+	}
 
-    public static void log(Level level, String message){
-        LOGGER.log(level, "["+MOD_NAME+"] " + message);
-    }
+	public static void log(Level level, String message){
+		LOGGER.log(level, "["+MOD_NAME+"] " + message);
+	}
 
 }
